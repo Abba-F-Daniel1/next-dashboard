aa5eced4e77d9d8bcb925e6a5160945f6f3dd713
Abba-F-Daniel1 <abbafrederickdaniel@yahoo.com>
2024-02-11 23:39:58 +0100

    completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next'; 
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -5,6 +6,9 @@ import styles from './ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+export const metadata: Metadata = {
+  title: 'Welcome',
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
